3e533fd5a6db017411bf8345f003a4c11d768ec6
Ayush <abhay2ayushdsma@gmail.com>
2023-11-05 23:37:41 +0530

    uploading relevant files

---
+++ .ipynb_checkpoints/news_research_tool-checkpoint.py
@@ -0,0 +1,33 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "3f05d06b-b31d-4bad-b449-09c276a4e4ad",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "env_for_llm",
+   "language": "python",
+   "name": "env_for_llm"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.11.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
